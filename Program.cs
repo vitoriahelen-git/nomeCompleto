1157e27b3b9faa9a191f60e010a6f3009f0a219c
vitoriahelen-git <vitoriahellen3004@gmail.com>
2020-09-12 00:37:17 -0300

    Resolução do exercicio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace nomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome; 
+            string sobrenome; 
+            Console.Write("Digite seu nome: ");
+             nome = Console.ReadLine(); 
+            Console.Write("Digite o seu sobrenome: "); 
+            sobrenome = Console.ReadLine(); 
+            Console.WriteLine("\nNome Completo: "+nome + " " + sobrenome);
+            Console.WriteLine("Nome de catálogo: "+sobrenome.ToUpper() + ", " + nome );
+
+            
+        }
+    }
+}
